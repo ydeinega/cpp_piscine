6bbab7d30d6a9f39298a10bbfdebea9e47ef1781
Yuliia Deineha <ydeineha@e1z3r1p22.42.us.org>
2018-10-03 21:45:14 -0700

    ex07 done

--- d01/ex07/main.cpp
+++ d01/ex07/main.cpp
@@ -14,8 +14,30 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <locale> 
 
-void	sed_file(std::ifstream & file, std::string s1, std::string s2)
+void		write_to_file(std::string content, char *name)
+{
+	std::string file_name;
+	std::locale loc;
+
+	file_name = name;
+	for (std::string::size_type i = 0; i < file_name.size(); i++)
+		file_name[i] = toupper(file_name[i], loc);
+	file_name += ".replace";
+
+	std::cout << file_name <<std::endl;
+
+	std::ofstream file(file_name);
+	if (!file.is_open())
+	{
+		std::cout << "The file to write output could not be opened" << std::endl;
+		exit (0);
+	}	
+	file << content;
+}
+
+std::string	sed_file(std::ifstream & file, std::string s1, std::string s2)
 {
 	std::stringstream	ss;
 	std::size_t			found;
@@ -35,14 +57,14 @@ void	sed_file(std::ifstream & file, std::string s1, std::string s2)
 		std::cout << "String1 not found in the file" << std::endl;
 		exit (0);
 	}
-	std::ofstream file_2("test2");
-	file_2 << content;
+	return (content);
 }
 
-int		main(int argc, char **argv)
+int			main(int argc, char **argv)
 {
 	std::string s1;
 	std::string s2;
+	std::string content;
 
 	if (argc == 4)
 	{
@@ -54,13 +76,18 @@ int		main(int argc, char **argv)
 			std::cout << "The file could not be opened" << std::endl;
 			exit (0);
 		}
-		//check is the file is empty
+		if (file.peek() == std::ifstream::traits_type::eof())
+		{
+			std::cout << "The file is empty" << std::endl;
+			exit (0);
+		}
 		if (s1.empty() || s2.empty())
 		{
 			std::cout << "One of the strings or both strings are empty" << std::endl;
 			exit (0);
 		}
-		sed_file(file, s1, s2);
+		content = sed_file(file, s1, s2);
+		write_to_file(content, argv[1]);
 	}
 	else
 		std::cout << "Usage:./program filename string1 string2" << std::endl;
